c7bcd960d75c26f1eb3af1d936d725543c9ac039
tuyentac95 <66375774+tuyentac95@users.noreply.github.com>
2020-06-08 11:33:00 +0700

    add file thuc hanh bien và toan tu

---
+++ thuchanh_toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeigth;
+inputWidth = prompt("Enter the width");
+inputHeigth = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeigth);
+let area = width*height;
+document.write("The area is: "+area);
